8a89d4d008b28f57b02293e12f71f8d2656eb7af
huay <huayun@dpark.com.cn>
2018-12-11 10:33:42 +0800

    更新服务启动项

--- Geone.JCXX/WebService/Geone.JCXX.WebService/Module/BaseModule.cs
+++ Geone.JCXX/WebService/Geone.JCXX.WebService/Module/BaseModule.cs
@@ -1,5 +1,4 @@
 ﻿using Geone.Utiliy.Build;
-using Geone.Utiliy.Component;
 using Geone.Utiliy.Library;
 using Geone.Utiliy.Logger;
 using Microsoft.Extensions.Configuration;
@@ -12,7 +11,7 @@ namespace Geone.JCXX.WebService
         /// <summary>
         /// 访问服务的客户端信息
         /// </summary>
-        protected AppInitialize Init { get; set; }
+        protected AppSettingsConfig Init { get; set; }
 
         /// <summary>
         /// 应用用户信息
@@ -23,8 +22,8 @@ namespace Geone.JCXX.WebService
         {
             Get("/", r =>
             {
-                AppInitialize Init = ctool.Get().Get<AppInitialize>();
-                return RepModel.SuccessAsJson($"欢迎使用{Init.Name}，本服务为{Init.Desc}。");
+                AppSettingsConfig Init = ctool.Get().Get<AppSettingsConfig>();
+                return RepModel.SuccessAsJson($"欢迎使用{Init.ApplicationName}，本服务为{Init.ApplicationDesc}。");
             });
         }
     }
